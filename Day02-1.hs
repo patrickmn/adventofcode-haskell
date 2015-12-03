d981bd2d1adbf05d44f120f9652672061b55c73f
Patrick Mylund Nielsen <patrick@patrickmn.com>
2015-12-03 16:01:16 -0500

    Cleaner Day02-1.hs

--- Day02-1.hs
+++ Day02-1.hs
@@ -9,13 +9,10 @@ main = do
     input <- trim <$> readFile "day02-input.txt"
     print $ foldr (f . parse) 0 (lines input)
   where
-    f (l, w, h) acc = acc + surface + smallest
+    f (l, w, h) acc = acc + surface + minimum sides
       where
-        surface   = (2 * side1) + (2 * side2) + (2 * side3)
-        side1     = l * w
-        side2     = w * h
-        side3     = h * l
-        smallest  = minimum [side1, side2, side3]
+        surface = sum $ map (2 *) sides
+        sides   = [l*w, w*h, h*l]
 
 parse :: String -> (Int, Int, Int)
 parse s = (read l, read w, read h)

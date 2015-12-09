b750d041e27b888fbe560ff688c7588af6a806c0
Patrick Mylund Nielsen <patrick@patrickmn.com>
2015-12-09 13:51:43 -0500

    Tidy Day09

--- src/AdventofCode/Day09.hs
+++ src/AdventofCode/Day09.hs
@@ -7,17 +7,16 @@ day09a :: String -> Int
 day09a = minimum . day09
 
 day09 :: String -> [Int]
-day09 input = routeDists
+day09 input = map routeSum possibleRoutes
   where
-    routeDists     = map routeSum possibleRoutes
     routeSum locs  = sum $ map (dist H.!) (zip locs (tail locs))
     possibleRoutes = permutations $ nub $ map fst $ H.keys dist
     dist           = H.fromList $ concatMap (parse . words) $ lines input
 
 parse :: [String] -> [((String, String), Int)]
-parse [x, "to", y, "=", nstr] = [((x, y), n), ((y, x), n)]
+parse [x, "to", y, "=", n] = [((x, y), rn), ((y, x), rn)]
   where
-    n = read nstr
+    rn = read n
 parse _                    = error "no parse"
 
 day09b :: String -> Int

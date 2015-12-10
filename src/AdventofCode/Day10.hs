eafe3906753ea2c5846ab372fd74d53d8729fa06
Patrick Mylund Nielsen <patrick@patrickmn.com>
2015-12-10 00:30:04 -0500

    Use iterate for the for loop in Day 10

--- src/AdventofCode/Day10.hs
+++ src/AdventofCode/Day10.hs
@@ -3,17 +3,12 @@ module AdventofCode.Day10 (day10a, day10b) where
 import Data.List (group)
 
 day10a :: String -> Int
-day10a = length . loop 40
+day10a = length . (!! 40) . iterate lookAndSay
 
-loop :: Int -> String -> String
-loop n = go 0
-  where
-    go cur input = if cur == n then input else go (succ cur) (encode input)
-
-encode :: String -> String
-encode = concatMap f . group
+lookAndSay :: String -> String
+lookAndSay = concatMap f . group
   where
     f g = show (length g) ++ [head g]
 
 day10b :: String -> Int
-day10b = length . loop 50
+day10b = length . (!! 50) . iterate lookAndSay

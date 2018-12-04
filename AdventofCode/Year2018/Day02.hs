9418bc3d637306d6d8315dff43bc1d536e44b2fc
Patrick Mylund Nielsen <patrick@patrickmn.com>
2018-12-03 19:42:58 -0500

    2018-day02b

--- AdventofCode/Year2018/Day02.hs
+++ AdventofCode/Year2018/Day02.hs
@@ -10,4 +10,19 @@ day02a input = numTwo * numThree
     grouped  = map (group . sort) $ lines input
 
 day02b :: String -> String
-day02b input = undefined
+day02b input = case filter ((== 1) . length . snd) splits of
+    []         -> error "day02b: not found"
+    ((c, _):_) -> c
+  where
+    splits = concatMap (\x -> map (splitLetters x) ids) ids
+    ids    = lines input
+
+splitLetters :: String -> String -> (String, String)
+splitLetters = go ("", "")
+  where
+    go (c, uc) []     []     = (reverse c, reverse uc)
+    go (c, uc) []     (y:ys) = go (c, y:uc) [] ys
+    go (c, uc) (x:xs) []     = go (c, x:uc) xs []
+    go (c, uc) (x:xs) (y:ys)
+        | x == y    = go (x:c, uc) xs ys
+        | otherwise = go (c, x:uc) xs ys
